75996c203c107d7fc542ac34e80d7276fd65a22b
Levi Smith <levi@fynx.me>
2016-04-11 10:07:30 -0500

    adding variable export template (source.sh) updating attributes

---
+++ source.sh
@@ -0,0 +1,19 @@
+# Incident Bot Exports
+export HUBOT_NAME='incident-bot'
+export HUBOT_SOURCE='https://github.com/github/hubot.git'
+export HUBOT_VERSION='2.18.0'
+export HUBOT_DIR='/opt/incident_bot'
+export HUBOT_USER='hubot'
+export HUBOT_GROUP='hubot'
+export HUBOT_DAEMON='runit'
+export RUNIT_LOGGER='true'
+
+# Slack Exports
+export SLACK_TOKEN=
+
+# Pagerduty Exports
+export PAGERDUTY_API_KEY=
+export PAGERDUTY_SERVICE_API_KEY=
+export PAGERDUTY_SUBDOMAIN=
+export PAGERDUTY_USER_ID=
+export PAGERDUTY_SERVICES=
